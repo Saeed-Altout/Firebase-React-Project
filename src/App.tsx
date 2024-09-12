d7c0060a2896255e20ec08c3b9003c2d5c3ac4fb
Saeed Altout <saeedaltoutpro@gmail.com>
2024-09-12 17:32:06 +0300

    1- create read page and completed function read data in firebase. 2- install react-spinners.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Write from "./components/pages/Write";
+
 import Home from "./components/pages/Home";
+import Write from "./components/pages/Write";
+import Read from "./components/pages/Read";
 
 export default function App() {
   return (
@@ -9,6 +11,7 @@ export default function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/write" element={<Write />} />
+          <Route path="/read" element={<Read />} />
         </Routes>
       </Router>
     </div>
---
+++ src/components/pages/Read.tsx
@@ -0,0 +1,51 @@
+import * as React from "react";
+import app from "../../firebaseConfig";
+import { getDatabase, ref, get } from "firebase/database";
+import { BeatLoader } from "react-spinners";
+
+export default function Read() {
+  const [notifications, setNotifications] = React.useState<
+    { notification: string }[]
+  >([]);
+  const [isLoading, setIsLoading] = React.useState(false);
+
+  const fetch = async () => {
+    const db = getDatabase(app);
+    const dbRef = ref(db, "notifications/");
+    try {
+      setIsLoading(true);
+      const snapshot = await get(dbRef);
+      if (snapshot.exists()) {
+        setNotifications(Object.values(snapshot.val()));
+      } else {
+        setNotifications([]);
+      }
+      console.log("Data fetch successfully!");
+    } catch (error) {
+      console.error("Error fetching data: ", error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <div className="flex justify-center items-center h-screen w-full flex-col gap-y-5">
+      <button
+        onClick={() => fetch()}
+        className="bg-purple-700 text-white px-4 py-2 rounded-md"
+      >
+        {isLoading ? <BeatLoader color="#fff" size={10} /> : "Display Data"}
+      </button>
+      <ul className="border rounded-md bg-purple-50 p-10 w-full max-w-5xl grid grid-cols-1 md:grid-cols-2 gap-10">
+        {notifications.map((notification, index) => (
+          <li
+            key={index}
+            className="bg-white px-4 py-6 text-purple-950/90 capitalize font-semibold"
+          >
+            {notification?.notification}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
